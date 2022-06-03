5942ec2c51fc185666f6900c42192769e2d2b357
Andrew Trice <amtrice@us.ibm.com>
2022-06-03 17:18:01 -0400

    regen from latest boms (#4)
    * regen from latest boms
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- 210-azure-portworx-storage/terraform/main.tf
+++ 210-azure-portworx-storage/terraform/main.tf
@@ -1,25 +1,26 @@
 module "azure-portworx" {
-  source = "github.com/cloud-native-toolkit/terraform-azure-portworx?ref=v0.0.3"
+  source = "github.com/cloud-native-toolkit/terraform-azure-portworx?ref=v0.1.0"
 
   azure_client_id = var.azure_client_id
   azure_client_secret = var.azure_client_secret
   azure_subscription_id = var.azure_subscription_id
   azure_tenant_id = var.azure_tenant_id
   cluster_config_file = module.cluster.config_file_path
-  cluster_name = var.azure-portworx_cluster_name
   cluster_type = var.azure-portworx_cluster_type
   disk_size = var.azure-portworx_disk_size
+  enable_encryption = var.azure-portworx_enable_encryption
   kvdb_disk_size = var.azure-portworx_kvdb_disk_size
-  portworx_config = var.azure-portworx_portworx_config
+  portworx_spec = var.portworx_spec
+  portworx_spec_file = var.portworx_spec_file
   provision = var.azure-portworx_provision
   px_enable_csi = var.azure-portworx_px_enable_csi
   px_enable_monitoring = var.azure-portworx_px_enable_monitoring
-  region = var.region
-  resource_group_name = var.azure-portworx_resource_group_name
 }
 module "cluster" {
-  source = "github.com/cloud-native-toolkit/terraform-ocp-login?ref=v1.2.16"
+  source = "github.com/cloud-native-toolkit/terraform-ocp-login?ref=v1.3.1"
 
+  ca_cert = var.cluster_ca_cert
+  ca_cert_file = var.cluster_ca_cert_file
   cluster_version = var.cluster_cluster_version
   ingress_subdomain = var.cluster_ingress_subdomain
   login_password = var.cluster_login_password
